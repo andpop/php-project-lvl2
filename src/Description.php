37704e3ab505d31e97dbdb9821d81318393f7736
Andrey Popov <andpop@mail.ru>
2020-07-02 18:03:40 +0300

    Add step 1 and temporary new_tasks/

---
+++ src/Description.php
@@ -0,0 +1,24 @@
+<?php
+namespace Gendiff\Description;
+
+const DOC = <<<DOC
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff (-v|--version)
+
+Options:
+  -h --help                     Show this screen
+  -v --version                  Show version
+DOC;
+
+function showDescription()
+{
+    $args = \Docopt::handle(DOC, ['version' => "0.1"]);
+    /* var_dump($args); */
+    /* foreach ($args as $k=>$v) { */
+    /*     echo $k . ': ' . $v . PHP_EOL; */ 
+    /* } */
+}
+
